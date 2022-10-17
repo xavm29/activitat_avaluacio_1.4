51d19fae30e55320404d6e46333ce72c560c9c69
xavier <xavm29@gmail.com>
2022-10-17 18:09:12 +0200

    Incial projecte limpiar comentarios Primers pasos Implementos colors pero no se como es fan els hex Estructura app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,6 +9,20 @@ class Home extends StatelessWidget {
       appBar: AppBar (
         title:Text("Fitness Time"),
       ),
-    );
+      body:SingleChildScrollView (
+        child:Center(
+            child: Column(
+              children: [
+                Row (
+                    children: const Text('Hola Diana')
+                ),
+              ],
+            ),
+          )
+
+
+        ,
+
+    )));
   }
 }
