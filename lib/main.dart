ea7fe322b2743babdc4c342b9d03c1f2b86e3ac5
xavier <xavm29@gmail.com>
2022-10-17 13:51:16 +0200

    Incial projecte limpiar comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        
-        
-        
-        
-        
-        
-        
-        
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,15 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  
-  
-  
-
-  
-  
-  
-  
 
   final String title;
 
@@ -52,47 +33,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      
-      
-      
-      
-      
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
     return Scaffold(
       appBar: AppBar(
-        
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
-        
         child: Column(
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -109,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
