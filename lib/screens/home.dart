12b62205866eb99492cfe57c761c7b5f62d99404
xavier <xavm29@gmail.com>
2022-10-18 19:59:28 +0200

    Incial projecte limpiar comentarios Primers pasos Implementos colors pero no se como es fan els hex Estructura app Primera pantalla feta sense bottom navigation bar naviagation bar acabada Pantalla acabada menys color mas detalles

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,81 +7,110 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Fitness Time"),
-          actions:<Widget>[ CircleAvatar(
-        backgroundImage: NetworkImage("https://randomuser.me/api/portraits/women/44.jpg")
-          )],
-          leading: Icon(Icons.menu),
+      appBar: AppBar(
+        title: const Text("Fitness Time"),
+        actions: <Widget>[
+          CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://randomuser.me/api/portraits/women/44.jpg"))
+        ],
+        leading: Icon(Icons.menu),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hola diana",
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Text(
+                "Come 5 veces al día y permanece hidratada durante el dia",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              Text(
+                "Mas detalles",
+                style:Theme.of(context).textTheme.bodySmall,
 
-
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Hola diana",
-                  style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Utimas actividades",
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.directions_run),
+                  title: Text(
+                    "Running",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    "Ayer 19:20",
+                  ),
+                  trailing: Text(" 7200Km",
+                      style: Theme.of(context).textTheme.headline5),
                 ),
-                Text(
-                  "Come 5 veces al día y permanece hidratada durante el dia",
-                  style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.directions_run),
+                  title: Text(
+                    "Running",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    "Ayer 19:20",
+                  ),
+                  trailing: Text(" 7200Km",
+                      style: Theme.of(context).textTheme.headline5),
                 ),
-                Text(
-                  "Mas detalles",
-                  style: Theme.of(context).textTheme.bodySmall,
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.directions_run),
+                  title: Text(
+                    "Running",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    "Ayer 19:20",
+                  ),
+                  trailing: Text(" 7200Km",
+                      style: Theme.of(context).textTheme.headline5),
                 ),
-                SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  "Utimas actividades",
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.directions_run),
-                    title: Text(
-                      "Running",
-                      style: Theme.of(context).textTheme.headline6,
+              ),
+              Center(
+                child: Container(
+                  child: CircularPercentIndicator(
+                    radius: 60.0,
+                    lineWidth: 13.0,
+                    animation: true,
+                    percent: 0.7,
+                    center: Text(
+                      "70.0%",
+                      style: new TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
                     ),
-                    subtitle: Text(
-                      "Ayer 19:20",
+                    footer: Text(
+                      "Objetivo mensual",
+                      style: Theme.of(context).textTheme.headline5,
                     ),
-                    trailing: Text(" 7200Km",
-                        style: Theme.of(context).textTheme.headline5),
+                    circularStrokeCap: CircularStrokeCap.round,
+                    progressColor: Theme.of(context).primaryColor,
                   ),
                 ),
-                CircularPercentIndicator(
-                  radius: 120.0,
-                  lineWidth: 13.0,
-                  animation: true,
-                  percent: 0.7,
-                  center: Text(
-                    "70.0%",
-                    style: new TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                  footer: Text(
-                    "Sales this week",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 17.0),
-                  ),
-                  circularStrokeCap: CircularStrokeCap.round,
-                  progressColor: Colors.purple,
-                ),
-              ],
-
-
-            ),
-
-
+              ),
+            ],
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Inicio',
@@ -97,9 +126,8 @@ class Home extends StatelessWidget {
             label: 'Perfil',
             backgroundColor: Colors.purple,
           ),
-
-
-        ],),
+        ],
+      ),
     );
   }
 }
